324565acab7a62d4cf9778194716037c1f585331
semitrivial <alexander@math.ohio-state.edu>
2014-11-02 13:23:05 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var animation_running = 0;
+
+function divclicked(x)
+{
+  if ( animation_running == 1 )
+    return;
+
+  animation_running = 1;
+
+  $(".tabs").each(function()
+  {
+    if ( this.id == x )
+      $(this).show(250, function()
+      {
+        animation_running = 0;
+      });
+    else
+      $(this).hide(250);
+  });
+
+  document.getElementById("imagediv").className = "nine columns";
+  document.getElementById("rightdiv").className = "seven columns";
+}
+
+function image_click()
+{
+  document.getElementById("imagediv").className = "twelve columns";
+  document.getElementById("rightdiv").className = "four columns";
+}
