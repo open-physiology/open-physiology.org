b3ef3e2dd4a325a0347ff997e730680cf2f36796
semitrivial <alexander@math.ohio-state.edu>
2014-11-07 16:23:54 -0500

    Floating tiles

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var animation_running = 0;
 
+function g(x)
+{
+  return document.getElementById(x);
+}
+
 function divclicked(x)
 {
   if ( animation_running == 1 )
@@ -17,4 +22,33 @@ function divclicked(x)
     else
       $(this).hide(250);
   });
+}
+
+function resize_divs()
+{
+  if ( window.innerWidth >= 767 )
+  {
+    g("rightdiv").style.height = (window.innerHeight-40) + "px";
+    g("imagediv").style.height = (window.innerHeight-120) + "px";
+
+    $("#overall_image").load(function()
+    {
+      center_image();
+    });
+
+    center_image();
+  }
+  else
+  {
+    g("rightdiv").style.height = "100%";
+    g("imagediv").style.height = "100%";
+    g("overall_image").style.marginTop = "0px";
+  }
+}
+
+function center_image()
+{
+  var ht = g("overall_image").height;
+
+  g("overall_image").style.marginTop = ((window.innerHeight-120-ht)/2)+"px";
 }
