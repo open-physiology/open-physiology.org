b1ac3aa46af16d580ca8beeffacbae3fa53aaec3
semitrivial <alexander@math.ohio-state.edu>
2014-12-15 10:29:18 -0500

    Rough draft youtubes support

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ var images =
   "OpenPhysiologySite_4.png"
 ];
 
+var youtubes =
+[
+  {
+    title: "Physiology Circuitboarding",
+    address: "https://www.youtube.com/embed/QtnrA25n19U",
+    thumbnail: "http://open-physiology.org/images/youtube1.jpg"
+  },
+  {
+    title: "ApiNATOMY Overview",
+    address: "https://www.youtube.com/embed/W8f551TQXjE",
+    thumbnail: "http://open-physiology.org/images/youtube2.jpg"
+  },
+  {
+    title: "ApiNATOMY &amp; the Human Protein Atlas",
+    address: "https://www.youtube.com/embed/MlmIcdlYjLk",
+    thumbnail: "http://open-physiology.org/images/youtube3.webp"
+  }
+];
+
 var animation_running = 0;
 var left_taken_over = 0;
 
@@ -195,6 +214,35 @@ function init_slideshow()
   {
     update_slideshow();
   });
+}
+
+function init_youtube()
+{
+  var youtube_html = "";
+  var footer_html = "";
+
+  for ( var i = 0; i < youtubes.length; i++ )
+  {
+    youtube_html +=
+      "<div id='youtube" + i + "'" + (i==0 ? "" : " style='display:none'") + ">" +
+        "<iframe width='560' height='315' src='" + youtubes[i].address  + "' frameborder='0' allowfullscreen></iframe>" +
+      "</div>";
+
+    footer_html +=
+      "<li style='display:inline'>" +
+        "<ul>" +
+          "<li>" +
+            "<img src='" + youtubes[i].thumbnail + "'>" +
+          "</li>" +
+          "<li>" +
+            youtubes[i].title +
+          "</li>" +
+        "</ul>" +
+      "</li>";
+  }
+
+  g("youtube_holder").innerHTML = youtube_html;
+  g("youtube_footer").innerHTML = footer_html;
 }
 
 function update_slideshow()
